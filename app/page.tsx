75cecda1b590d1edbaa579149cacfe8994669d24
aakashsaini09 <aakashsaini948585@gmail.com>
2025-05-10 15:28:20 +0530

    basic prep

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <Button>Button</Button>
+    </div>
   );
 }
