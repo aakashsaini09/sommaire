b60e8000edf379cc5088cc30d001d94e1622aed2
aakashsaini09 <aakashsaini948585@gmail.com>
2025-05-13 12:56:42 +0530

    bg-gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import BgGradient from "@/components/common/bg-gradient";
 import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
     <div className="relative w-full h-full">
-      <HeroSection/>
+      <div className="flex flex-col">
+      <BgGradient/>
+        <HeroSection/>
+      </div>
       {/* <DemoSection/> */}
       {/* <HowItWorkSection/>
       <PricingSection/>
--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -5,15 +5,20 @@ import Link from "next/link";
 
 export default function HeroSection() {
     return <section className="relative flex flex-col items-center w-full justify-center lg:px-12 py-16 sm:py-20 lg:pb-28 transition-all animate-in text-center bg-gradient-to-b from-white to-gray-50">
-        <div className="">
                 <div className="relative p-[1px] overflow-hidden rounded-full bg-linear-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x cursor-pointer group">
                     <Badge variant={"secondary"} className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors duration-200">
                         <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse"/>
                         <p className="text-base text-rose-600">Powered by AI</p>
                     </Badge>
                 </div>
-            </div>
-            <h1 className="font-bold py-6 text-center">Transform PDFs into concise summaries</h1>
+            <h1 className="font-bold py-6 text-center text-6xl text-black">
+                Transform PDFs into 
+                <span className="relative inline-block">
+                    <span className="relative z-10 px-2">concise</span>
+                    <span className="absolute inset-0 bg-rose-200/50 -rotate-2 rounded-lg transform -skew-y-1" aria-hidden="true"></span> 
+                </span>
+                summaries
+            </h1>
             <h2 className="text-lg sm:text-xl lg:text-2xl text-center px-4 lg:px-0 lg:max-w-4xl text-gray-600">Get a beautiful summary reel of the document in seconds.</h2>
             <div>
                 <Button variant={'link'} className="text-white mt-6 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-16">
