1da2e49cf0dba49e0dfd02738d1a74dd2cce8ff7
aakashsaini09 <aakashsaini948585@gmail.com>
2025-05-15 14:23:34 +0530

    demo-gradient

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import HeroSection from "@/components/home/hero-section";
 export default function Home() {
   return (
     <div className="relative w-full h-full">
-        <BgGradient/>
+        {/* <BgGradient/> */}
       <div className="flex flex-col">
         <HeroSection/>
         <DemoSection/>
