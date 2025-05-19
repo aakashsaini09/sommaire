b2dd1750372a37b5ec5841f014cf54a9d01543ef
aakashsaini09 <aakashsaini948585@gmail.com>
2025-05-19 12:53:30 +0530

    file upload done

--- components/upload/upload-form-input.tsx
+++ components/upload/upload-form-input.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 interface UploadFormInputProps {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
@@ -11,9 +12,18 @@ export default function UploadFromInput ({onSubmit}:
       <form 
         action=""
         className="flex flex-col gap-6"
-        onClick={onSubmit}>
-            <input type="file" />
-            <Button>Upload your PDF</Button>
+        onSubmit={onSubmit}>
+            <div className='flex justify-end items-center gap-1.5'>
+                <Input 
+                    type="file"
+                    id='file'
+                    name='file'
+                    accept='application/pdf'
+                    required
+                    className='cursor-pointer'
+                    />
+                <Button>Upload your PDF</Button>
+            </div>
         </form>
     </>
   )
--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,12 +1,32 @@
 'use client'
+import { z } from "zod";
 import UploadFromInput from "./upload-form-input";
-
+const schema = z.object({
+    file: z.instanceof(File, {message: "Invalid file"}).
+    refine((file) => file.size <= 20 * 1024 * 1024, 
+    "File size should be less than 20MB",
+    )
+    .refine((file) => file.type.startsWith('application/pdf'),
+    "File must be a PDF",
+    ),
+})
 export default function UploadForm() {
-    const handleSubmit = () => {
-        alert('form submitted')
-    }
-    return <div>
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const file = formData.get('file') as File;
+        // console.log(file);
+        // validating the file
+        const validatedFields = schema.safeParse({file});
+        console.log(validatedFields);
+        if(!validatedFields.success) {
+            console.log(validatedFields.error.flatten().fieldErrors.file?.[0] ?? 'Invalid file');
+            return;
+        }
+    };
+    return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
         <UploadFromInput onSubmit={handleSubmit}/>
     </div>
-    
+    )
 }
