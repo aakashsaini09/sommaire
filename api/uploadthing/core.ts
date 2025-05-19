b334b2503df961464e0ecc5abc0c82c79263ef19
aakashsaini09 <aakashsaini948585@gmail.com>
2025-05-19 13:13:06 +0530

    uploadthing api

---
+++ api/uploadthing/core.ts
@@ -0,0 +1,22 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing()
+
+export const outFileRouter = {
+    pdfUploader: f({ pdf: { maxFileSize: '32MB' } }).
+        middleware(
+            async ({ req }) => {
+                const user = await currentUser();
+                if (!user) throw new UploadThingError('Unauthrozied')
+                return { userId: user.id };
+            }
+        ).onUploadComplete(async ({ metadata, file }) => {
+            console.log('upload completed for user id', metadata.userId);
+            console.log("file url: ", file.url)
+            return { userId: metadata.userId, file };
+        })
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof outFileRouter;
